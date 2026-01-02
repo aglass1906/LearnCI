6722d26ed9eaca25603e6031e2655b7e2f185fc5
aglass <alan.glass.sankoa@gmail.com>
2026-01-02 13:02:06 -0500

    Initial Commit

---
+++ LearnCI/LearnCIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnCIApp.swift
+//  LearnCI
+//
+//  Created by Alan Glass on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LearnCIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
